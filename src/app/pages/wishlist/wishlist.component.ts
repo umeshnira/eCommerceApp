e1a3ffb2fd45c26b5c490fd3dd10f3667a6c599c
Geethu <geethukrishgs.777@gmail.com>
2020-05-14 19:33:43 +0530

    Added script for grid view in wishlist

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -9,5 +9,17 @@ export class WishlistComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  ngAfterViewInit() {
+    this.loadScript("assets/products/js/jquery-2.1.4.min.js");
+    this.loadScript("assets/products/js/carousel.js");
+  }
 
+  private loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement("script");
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
+  }
 }
