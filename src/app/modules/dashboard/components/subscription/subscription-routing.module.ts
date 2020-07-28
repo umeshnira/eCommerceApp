711d26011fcd85d4bbdd404c8073cbc8c3ff9e1c
noname <noname@noname.com>
2020-07-28 14:42:30 +0530

    Subscription Pages

--- src/app/modules/dashboard/components/subscription/subscription-routing.module.ts
+++ src/app/modules/dashboard/components/subscription/subscription-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SubscriptionComponent } from "./subscription.component";
+import { ViewSubscriberComponent } from "./view-subscriber/view-subscriber.component";
+import { SellerDetailsComponent } from "./seller-details/seller-details.component";
 const subscriptionRoutes = [
     {
         path: '',
         component: SubscriptionComponent
+    },
+    {
+        path: 'subscribers',
+        component: ViewSubscriberComponent
+    },
+    {
+        path: 'seller-details',
+        component: SellerDetailsComponent
     }
+    
 ];
 
 @NgModule({
--- src/app/modules/dashboard/components/subscription/subscription.module.ts
+++ src/app/modules/dashboard/components/subscription/subscription.module.ts
@@ -2,10 +2,12 @@ import { SharedModule } from "src/app/shared/shared-module";
 import { SubscriptionRoutingModule } from "./subscription-routing.module";
 import { NgModule } from "@angular/core";
 import { SubscriptionComponent } from "./subscription.component";
+import { ViewSubscriberComponent } from "./view-subscriber/view-subscriber.component";
+import { SellerDetailsComponent } from "./seller-details/seller-details.component";
 
 const modules = [SharedModule, SubscriptionRoutingModule];
 
-const components = [SubscriptionComponent];
+const components = [SubscriptionComponent, ViewSubscriberComponent,SellerDetailsComponent];
 
 const providers = [];
 
