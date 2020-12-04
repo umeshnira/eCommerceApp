074e89c6c0d3bea8e1d7db755925b51cb0aef350
BalaKrishnan <kishoreb@nirasystems.com>
2020-12-04 22:34:48 +0530

    Updated the image path in all html pages Include base url with vm url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   api: {
-    baseUrl: 'http://localhost:8100',
+    baseUrl: 'http://20.51.211.208:1337/ecommerce',
+    orderUrl: 'http://20.51.211.208:1338/ecommerce',
+    notificUrl: 'http://20.51.211.208:1339/ecommerce'
   }
 };
